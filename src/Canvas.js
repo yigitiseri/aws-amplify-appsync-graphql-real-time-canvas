2174fae64c4929ded7ea06eb4356974880467205
yigitiseri <yigitiseri@gmail.com>
2020-05-05 13:26:49 +0300

    Update Canvas.js

--- src/Canvas.js
+++ src/Canvas.js
@@ -25,8 +25,8 @@ function rand() {
 class Demo extends Component {
   state = {
     brushColor: rand(),
-    canvasHeight: 350,
-    canvasWidth: 380,
+    canvasHeight: 650,
+    canvasWidth: 700,
     brushRadius: 4,
     lazyRadius: 8
   }
